f771ea8158a43b2dabfa19118f6b32597fddba36
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2024-05-22 10:56:10 -0400

    Show object preview in level editor before spawning

--- Assets/Game/Scripts/LevelEditorManager.cs
+++ Assets/Game/Scripts/LevelEditorManager.cs
@@ -6,26 +6,33 @@ public class LevelEditorManager : Singleton<LevelEditorManager>
 {
     bool readyToSpawn = false;
     GameObject goToSpawn = null;
+    GameObject previewGO = null;
 
     private void Update()
     {
         if (!readyToSpawn || goToSpawn == null)
             return;
 
+        // Click -> Spawn the object
         if (Input.GetMouseButtonDown(0))
         {
             Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(mouseRay.origin, mouseRay.direction, out RaycastHit hitInfo, 100.0f))
             {
                 Instantiate(goToSpawn, hitInfo.point, Quaternion.Euler(0.0f, -180.0f, 0.0f));
+                if (previewGO != null)
+                    Destroy(previewGO);
                 readyToSpawn = false;
             }
         }
     }
 
-    public void StandbyToSpawn(GameObject objPrefab)
+    public void StandbyToSpawn(GameObject objPrefab, GameObject objPreviewPrefab)
     {
         readyToSpawn = true;
         goToSpawn = objPrefab;
+
+        // Show the image at the mouse position
+        previewGO = Instantiate(objPreviewPrefab, Vector3.zero, Quaternion.Euler(0.0f, -180.0f, 0.0f));
     }
 }
