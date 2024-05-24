594fd5718b44df43d017ba88aac28f29e0e94266
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2024-05-24 18:18:11 -0400

    Eye changes on slime (#16)
    * Change eyes on attack
    
    * Change slime eye on hit
    
    * Slime eye fixes

--- Assets/Game/Scripts/Enemy/Enemy.cs
+++ Assets/Game/Scripts/Enemy/Enemy.cs
@@ -1,25 +1,60 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public enum EnemyEye
+{
+    NORMAL,
+    ATTACK,
+    DEATH
+};
 
 [RequireComponent(typeof(SphereCollider))]
 public class Enemy : DamageTaker, ITakeDamage
 {
-    [SerializeField] Material[] slimeMaterials;
-    
-    float totalHealth = 0.0f;
+    [Header("Slime Eyes")]
+    [SerializeField] SkinnedMeshRenderer normalEye;
+    [SerializeField] SkinnedMeshRenderer attackEye;
+    [SerializeField] SkinnedMeshRenderer deathEye;
+
+    [SerializeField] float damageEyeTimer = 1.0f;
+    private bool freezeEyeChange = false;
 
     protected override void Start()
     {
         base.Start();
-        totalHealth = health;
+        normalEye.enabled = true;
+        attackEye.enabled = false;
+        deathEye.enabled = false;
     }
 
     public override void TakeDamage(Damage damage)
     {
         base.TakeDamage(damage);
-        //Debug.Log("Slime got damaged to " + health);
-        //foreach (Material mat in slimeMaterials)
-        //{
-        //}
+        StartCoroutine(ChangeEyeToDamage());
+    }
+
+    IEnumerator ChangeEyeToDamage()
+    {
+        SetEye(EnemyEye.DEATH);
+        freezeEyeChange = true;
+        yield return new WaitForSeconds(damageEyeTimer);
+        freezeEyeChange = false;
+    }
+
+    public void SetEye(EnemyEye enemyEye)
+    {
+        StartCoroutine(ChangeEye(enemyEye));
+    }
+
+    IEnumerator ChangeEye(EnemyEye enemyEye)
+    {
+        while (freezeEyeChange)
+        {
+            yield return null;
+        }
+        normalEye.enabled = (enemyEye == EnemyEye.NORMAL);
+        attackEye.enabled = (enemyEye == EnemyEye.ATTACK);
+        deathEye.enabled = (enemyEye == EnemyEye.DEATH);
     }
 }
