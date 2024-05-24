3b3b1f0015612ad2fdedeb12545361b3b146716d
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2024-05-24 14:55:22 -0400

    Changes in class structure and work on material change

--- Assets/Game/Scripts/Combat/DamageTaker.cs
+++ Assets/Game/Scripts/Combat/DamageTaker.cs
@@ -23,7 +23,7 @@ public class DamageTaker : MonoBehaviour, ITakeDamage
         Destroy(gameObject);
     }
 
-    public void TakeDamage(Damage damage)
+    public virtual void TakeDamage(Damage damage)
     {
         health -= damage.value;
         StartCoroutine(ApplyKnockback(damage));
--- Assets/Game/Scripts/Enemy/Enemy.cs
+++ Assets/Game/Scripts/Enemy/Enemy.cs
@@ -1,6 +1,25 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(SphereCollider))]
-public class Enemy : DamageTaker
+public class Enemy : DamageTaker, ITakeDamage
 {
+    [SerializeField] Material[] slimeMaterials;
+    
+    float totalHealth = 0.0f;
+
+    protected override void Start()
+    {
+        base.Start();
+        totalHealth = health;
+    }
+
+    public override void TakeDamage(Damage damage)
+    {
+        base.TakeDamage(damage);
+        //Debug.Log("Slime got damaged to " + health);
+        //foreach (Material mat in slimeMaterials)
+        //{
+        //}
+    }
 }
