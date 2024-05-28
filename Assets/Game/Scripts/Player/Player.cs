ec500cad795414bfc4d62007fd160cebcbb196f2
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2024-05-28 16:38:57 -0400

    Update Player health in UI (#21)

--- Assets/Game/Scripts/Player/Player.cs
+++ Assets/Game/Scripts/Player/Player.cs
@@ -5,4 +5,20 @@ using UnityEngine;
 [RequireComponent(typeof(CapsuleCollider))]
 public class Player : DamageTaker
 {
+    protected override void Start()
+    {
+        base.Start();
+
+        // Ensures that base.health does not change as per damage
+        isInvincible = true;
+    }
+
+    public override void TakeDamage(Damage damage)
+    {
+        // For player, we want to change GameManager.PlayerHealth
+        base.TakeDamage(damage);
+
+        GameManager.Instance.PlayerHealth -= damage.value;
+        StartCoroutine(ApplyKnockback(damage));
+    }
 }
