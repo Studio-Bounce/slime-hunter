329ac57e6d6926271ebcc96c613cf80d85ccab41
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2024-05-27 20:13:48 -0400

    Progress on rabbit

---
+++ Assets/Game/Scripts/AI/FSM/RabbitSlime/RabbitSlime_Cooldown.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Rabbit slime is vulnerable only in cooldown
+public class RabbitSlime_Cooldown : BasicSlime_Cooldown
+{
+
+}
