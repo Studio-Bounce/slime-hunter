d8c2e4f2ee45f612637f36f295d8ca077b850f4f
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2024-06-03 13:25:17 -0400

    Show red alert on damage

--- Assets/Game/Scripts/UI/HUDMenu.cs
+++ Assets/Game/Scripts/UI/HUDMenu.cs
@@ -6,11 +6,18 @@ using UnityEngine.UIElements;
 
 public class HUDMenu : Menu
 {
+    [Range(1, 100)][SerializeField] int maxAlert = 50;
+    [SerializeField] float damageAlertTime = 1.0f;
     ProgressBar healthProgressBar;
+    VisualElement healthDamageVE;
+
+    bool redAlertUp = false;
 
     void Start()
     {
         VisualElement root = uiDocument.rootVisualElement;
+
+        healthDamageVE = root.Q<VisualElement>("Red_Alert");
         VisualElement background = root.Q<VisualElement>("Background");
         VisualElement leftBg = background.Q<VisualElement>("Left");
         VisualElement leftStatusArea = leftBg.Q<VisualElement>("StatusArea");
@@ -24,9 +31,33 @@ public class HUDMenu : Menu
 
     void UpdateHealthBar(int health)
     {
+        // Damage red alert UI
+        if (!redAlertUp)
+        {
+            StartCoroutine(InitiateRedAlert());
+        }
+
         if (healthProgressBar != null)
         {
             healthProgressBar.value = (float)health / GameManager.Instance.playerMaxHealth;
         }
+    }
+
+    IEnumerator InitiateRedAlert()
+    {
+        redAlertUp = true;
+        // Set red alert to maxAlert. Reduce it to 0 over time
+        int opacity = maxAlert;
+        healthDamageVE.style.opacity = opacity;
+        float timeDelta = damageAlertTime / maxAlert;
+
+        while (opacity > 0)
+        {
+            --opacity;
+            healthDamageVE.style.opacity = opacity;
+
+            yield return new WaitForSeconds(timeDelta);
+        }
+        redAlertUp = false;
     }
 }
