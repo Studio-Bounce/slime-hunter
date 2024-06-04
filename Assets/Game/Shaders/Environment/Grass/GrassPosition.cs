86c4abad19bfe8408c27d92c075b83a757ad6903
RiccioLi <liqingao480@gmail.com>
2024-06-04 14:01:19 -0400

    Squashed commit of the following:
    commit b3623b5f673a92daba659c3e698ad4b4f1502f2a
    Author: RiccioLi <liqingao480@gmail.com>
    Date:   Tue Jun 4 14:01:01 2024 -0400
    
        demolevel grass
    
    commit 27a97744fb7d6710c20c005acfb621f66aa6a9fe
    Merge: e8a314b c7d8f28
    Author: RiccioLi <liqingao480@gmail.com>
    Date:   Tue Jun 4 13:54:36 2024 -0400
    
        Merge branch 'develop' into origin/Riccio/Art_test
    
    commit e8a314b0149b1775394600d654d2ac220ab36633
    Author: RiccioLi <liqingao480@gmail.com>
    Date:   Tue Jun 4 13:53:57 2024 -0400
    
        grass in terrian
    
    commit e517af3f2236f095d44351ed1aefd97e5eb625f7
    Author: RiccioLi <157414111+RiccioLi@users.noreply.github.com>
    Date:   Mon Jun 3 22:51:24 2024 -0400
    
        save
    
    commit 6a6910d020f94a03dfc8457ee192817b8a3ac0eb
    Merge: 25d134e 57bd07e
    Author: RiccioLi <157414111+RiccioLi@users.noreply.github.com>
    Date:   Mon Jun 3 22:43:50 2024 -0400
    
        Merge branch 'develop' into origin/Riccio/Art_test
    
    commit 25d134e53921c43e99451c6236646268a3d85261
    Author: RiccioLi <157414111+RiccioLi@users.noreply.github.com>
    Date:   Mon Jun 3 22:42:38 2024 -0400
    
        no interaction grass decided
    
    commit f1d8dc07044123183670cb91c55f562b3abe786d
    Author: RiccioLi <liqingao480@gmail.com>
    Date:   Mon Jun 3 21:47:10 2024 -0400
    
        Grassshader
    
    commit d0b93bd9b6c7d2879847774e2e9382af28ddb741
    Author: RiccioLi <liqingao480@gmail.com>
    Date:   Mon Jun 3 19:41:07 2024 -0400
    
        remove missing
    
    commit 2be34d350765958eab588c6418024533de5d44b3
    Author: RiccioLi <liqingao480@gmail.com>
    Date:   Mon Jun 3 18:50:47 2024 -0400
    
        GRASS
    
    commit 762ef3bfb2fc77eaa44852654fe82c95332f0580
    Author: RiccioLi <liqingao480@gmail.com>
    Date:   Mon Jun 3 17:00:43 2024 -0400
    
        grass model
    
    commit bfb5c1670ffc3d0ff9ad25112e1dd2487431f2e0
    Author: RiccioLi <liqingao480@gmail.com>
    Date:   Mon Jun 3 15:09:45 2024 -0400
    
        Delete all sky box
    
    commit 58cb466097c20b4f21197f3d695c2ecbce4c0d47
    Merge: 3a5ba13 4996466
    Author: RiccioLi <liqingao480@gmail.com>
    Date:   Mon Jun 3 14:37:52 2024 -0400
    
        Merge branch 'develop' into origin/Riccio/Art_test
    
    commit 3a5ba136917fc214d91ea49623c805e10e615b1e
    Author: RiccioLi <liqingao480@gmail.com>
    Date:   Mon Jun 3 14:36:58 2024 -0400
    
        Add several sky box

---
+++ Assets/Game/Shaders/Environment/Grass/GrassPosition.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassPosition : MonoBehaviour
+
+{
+    private GameObject tracker;
+
+    private Material grassMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grassMat = GetComponent<Renderer>().material;
+        tracker = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 trackerPos = tracker.GetComponent<Transform>().position;
+        grassMat.SetVector("_trackerPosition", trackerPos);
+    }
+}
