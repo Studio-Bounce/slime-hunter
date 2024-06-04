b3623b5f673a92daba659c3e698ad4b4f1502f2a
RiccioLi <liqingao480@gmail.com>
2024-06-04 14:01:01 -0400

    demolevel grass

--- Assets/Game/Shaders/Environment/GrassPosition.cs
+++ Assets/Game/Shaders/Environment/GrassPosition.cs
@@ -1,28 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class GrassPosition : MonoBehaviour
+//public class GrassPosition : MonoBehaviour
 
-{
-    private GameObject tracker;
+//{
+ //   private GameObject tracker;
 
-    private Material grassMat;
+//    private Material grassMat;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        grassMat = GetComponent<Renderer>().material;
-        tracker = GameObject.Find("Player");
-    }
+//    void Start()
+  //  {
+ //       grassMat = GetComponent<Renderer>().material;
+ //       tracker = GameObject.Find("Player");
+  //  }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (tracker != null)
-        {
-            Vector3 trackerPos = tracker.GetComponent<Transform>().position;
-            grassMat.SetVector("_trackerPosition", trackerPos);
-        }
-    }
-}
+  //  void Update()
+   // {
+   //     if (tracker != null)
+  //      {
+   //         Vector3 trackerPos = tracker.GetComponent<Transform>().position;
+   //         grassMat.SetVector("_trackerPosition", trackerPos);
+   //     }
+   // }
+//}
