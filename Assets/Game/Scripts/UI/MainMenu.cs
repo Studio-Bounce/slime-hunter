0a52072f7ba3c819b18e1e176198a6113aea70c4
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2024-06-11 18:45:06 -0400

    Make continue and new game buttons work

--- Assets/Game/Scripts/UI/MainMenu.cs
+++ Assets/Game/Scripts/UI/MainMenu.cs
@@ -2,29 +2,46 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class MainMenu : Menu
 {
     public string playSceneName;
-    public string menuSceneName; //Main menu Scene
+    public string menuSceneName;
 
     void Start()
     {
         VisualElement root = uiDocument.rootVisualElement;
         Label lblPlay = root.Q<Label>("lblPlay");
+        Label lblContinue = root.Q<Label>("lblContinue");
         Label lblQuit = root.Q<Label>("lblQuit");
         lblPlay.RegisterCallback<ClickEvent>(ev => StartGame());
+        lblContinue.RegisterCallback<ClickEvent>(ev => ContinueGame());
         lblQuit.RegisterCallback<ClickEvent>(ev => QuitGame());
     }
 
     private void StartGame()
     {
-        SceneLoader.Instance.UnloadScene(menuSceneName);//unload menu
+        SceneLoader.Instance.UnloadScene(menuSceneName);
         SceneLoader.Instance.LoadScene(playSceneName);
         UIManager.Instance.SetMainMenu(false);
         UIManager.Instance.SetHUDMenu(true);
         GameManager.Instance.GameState = GameStates.GAMEPLAY;
+    }
+
+    private void ContinueGame()
+    {
+        SceneLoader.Instance.UnloadScene(menuSceneName);
+        SceneLoader.Instance.LoadScene(playSceneName, callback: LoadData);
+        UIManager.Instance.SetMainMenu(false);
+        UIManager.Instance.SetHUDMenu(true);
+        GameManager.Instance.GameState = GameStates.GAMEPLAY;
+    }
+
+    void LoadData(AsyncOperation _, string sceneName)
+    {
+        PersistenceManager.Instance.LoadGame();
     }
 
     private void QuitGame()
