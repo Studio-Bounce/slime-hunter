ce190aab50b425c9db45d9339508948cc1abef84
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2024-06-21 17:01:59 -0400

    Fix jump

--- Assets/Game/Scripts/JumpArea.cs
+++ Assets/Game/Scripts/JumpArea.cs
@@ -6,8 +6,18 @@ public class JumpArea : MonoBehaviour
 {
     [SerializeField] float jumpForce = 10.0f;
     [SerializeField] float jumpDuration = 0.5f;
-    [Tooltip("Jump targets must be present outside this object's collider area")]
-    [SerializeField] Transform[] jumpTargets;
+    
+    public Transform[] jumpTargets;
+
+    private void Start()
+    {
+        jumpTargets = new Transform[transform.childCount];
+        int i = 0;
+        foreach (Transform child in transform)
+        {
+            jumpTargets[i++] = child;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,15 +26,15 @@ public class JumpArea : MonoBehaviour
             // Apply jump
             if (other.gameObject.TryGetComponent<PlayerController>(out var controller))
             {
-                // Check which target is furthest, the player will jump there
+                // Check which target is closest, the player will jump there
                 Vector3 target = other.transform.position;
-                float maxDist = 0;
+                float minDist = 100000f;
                 foreach (Transform jumpTarget in jumpTargets)
                 {
                     float distance = Vector3.Distance(other.gameObject.transform.position, jumpTarget.position);
-                    if (distance > maxDist)
+                    if (distance < minDist)
                     {
-                        maxDist = distance;
+                        minDist = distance;
                         target = jumpTarget.position;
                     }
                 }
