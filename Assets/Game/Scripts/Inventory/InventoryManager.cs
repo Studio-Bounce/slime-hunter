330b097b7b407345b080c6153782a54d25936930
Hylu <roylu99@gmail.com>
2024-07-08 14:59:05 -0400

    Add binary writing

---
+++ Assets/Game/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class InventoryManager : Singleton<InventoryManager>
+{
+    public Inventory inventory;
+
+    void Start()
+    {
+        inventory = new Inventory();
+        inventory.maxWeight = 20;
+    }
+
+    public int GetTotalWeight()
+    {
+        int totalValue = 0;
+        foreach (Item item in inventory.items)
+        {
+            totalValue += item.weight * item.quantity;
+        }
+        return totalValue;
+    }
+
+    public bool AddItem(Item item)
+    {
+        if (GetTotalWeight() > inventory.maxWeight) // TODO: Inefficient
+        {
+            inventory.items.Add(item);
+            return true;
+        }
+        return false;
+    }
+
+    public void RemoveItem(Item item)
+    {
+        inventory.items.Remove(item);
+    }
+
+    public void UseItem(Item item)
+    {
+
+    }
+}
