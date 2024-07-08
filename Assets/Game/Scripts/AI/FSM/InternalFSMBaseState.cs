2c3f4e397baceb7939ece973b167191ac4571181
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2024-07-08 12:07:12 -0400

    Revert "Combat mechanics + basic level (v1 of playtesting) (#40)"
    This reverts commit 5eaea26d35df5b4ae4ede337416ee78c10532ebe.

--- Assets/Game/Scripts/AI/FSM/InternalFSMBaseState.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class InternalFSMBaseState : StateMachineBehaviour
-{
-    abstract public void Init(GameObject _owner, FSM _fsm);
-}
