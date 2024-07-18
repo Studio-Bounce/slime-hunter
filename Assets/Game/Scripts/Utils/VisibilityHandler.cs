8a0b6ba302f99947c88157487f4d0823524076cc
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2024-07-18 11:36:37 -0400

    Improve visibility handler

--- Assets/Game/Scripts/Utils/VisibilityHandler.cs
+++ Assets/Game/Scripts/Utils/VisibilityHandler.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -9,30 +10,39 @@ public class VisibilityHandler : MonoBehaviour
     public UnityEvent OnInvisible;
 
     SkinnedMeshRenderer[] skinnedMeshRenderers;
-    bool isVisible;
+    bool isVisible, firstTime;
+    float timeElapsed = 0.0f;
+    const float TIME_DELAY = 1.0f;
 
     private void Start()
     {
         skinnedMeshRenderers = GetComponentsInChildren<SkinnedMeshRenderer>();
         isVisible = true;
+        firstTime = true;
+        timeElapsed = 0.0f;
     }
 
     private void Update()
     {
         if (!CameraManager.ActiveCamera || !CameraManager.ActiveCamera.enabled)
             return;
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed < TIME_DELAY)
+            return;
 
         bool visibility = IsVisible(CameraManager.ActiveCamera);
-        if (visibility && !isVisible)
+        if (visibility && (!isVisible || firstTime))
         {
             isVisible = true;
             OnVisible?.Invoke();
         }
-        else if (!visibility && isVisible)
+        else if (!visibility && (isVisible || firstTime))
         {
             isVisible = false;
             OnInvisible?.Invoke();
         }
+        firstTime = false;
+        timeElapsed = 0.0f;
     }
 
     bool IsVisible(Camera camera)
