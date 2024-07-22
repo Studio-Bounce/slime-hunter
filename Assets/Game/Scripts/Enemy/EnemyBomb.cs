6e7146cee90d06e9633acb5d76daeda620e3e8ae
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2024-07-22 15:22:27 -0400

    More bomber changes (#155)

--- Assets/Game/Scripts/Enemy/EnemyBomb.cs
+++ Assets/Game/Scripts/Enemy/EnemyBomb.cs
@@ -12,6 +12,7 @@ public class EnemyBomb : DamageDealer
     struct ShaderParam
     {
         public string name;
+        public MeshRenderer renderer;
         public float startValue;
         public float endValue;
     };
@@ -40,7 +41,6 @@ public class EnemyBomb : DamageDealer
         sphereCollider.radius = 0.1f;  // start off small
         Active = false;
         didExplode = false;
-        groundCrackMesh = groundCrack.GetComponent<MeshRenderer>();
         groundCrack.gameObject.SetActive(false);
     }
 
@@ -71,7 +71,7 @@ public class EnemyBomb : DamageDealer
             foreach (ShaderParam _param in shaderParams)
             {
                 float value = _param.startValue + (_param.endValue - _param.startValue) * t;
-                groundCrackMesh.material.SetFloat(_param.name, value);
+                _param.renderer.material.SetFloat(_param.name, value);
             }
 
             yield return null;
