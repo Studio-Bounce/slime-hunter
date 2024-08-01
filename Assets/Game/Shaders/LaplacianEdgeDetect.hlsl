58db8b9476020188a117e10eb7fc1916f6a7a147
Connor Switzer <conswitzer@gmail.com>
2024-08-01 10:49:25 -0400

    WIP2

--- Assets/Game/Shaders/LaplacianEdgeDetect.hlsl
+++ Assets/Game/Shaders/LaplacianEdgeDetect.hlsl
@@ -1,10 +1,14 @@
+
 #ifndef LAPLACIAN_EDGE_DETECTION_INCLUDED
 #define LAPLACIAN_EDGE_DETECTION_INCLUDED
 
+float LinearEyeDepth( float z ){
+    return 1.0 / (_ZBufferParams.z * z + _ZBufferParams.w);
+}
 
 void LaplacianEdgeDetect_float(float2 UV, float2 TexelSize, float KernelSize, UnityTexture2D viewNormals, SamplerState ss, out float4 Matte)
 {
-    float ks = 5.0;
+    float ks = 7.0;
 
     float3 filteredNormals = float3(0.0, 0.0,0.0);
     float filteredDepth = 0.0;
@@ -19,46 +23,30 @@ void LaplacianEdgeDetect_float(float2 UV, float2 TexelSize, float KernelSize, Un
     float hks = floor(ks/2);
     float maxLoops = ksf * ksf;
 
+    float3 sN;
+    float sD;
     
     [unroll(1024)]
     for (float i = 0; i < maxLoops; i++) {
         x = floor(i % ks - hks);
         y = floor(floor(i / ks) - hks);
 
-        if (x != 0.0 || y != 0.0){
-                PixelUVs = UV + TexelSize * float2(x,y);
+        PixelUVs = UV + TexelSize * float2(x,y);
+        sN = viewNormals.tex.SampleLevel(ss, PixelUVs, 0).rgb;
+        sD = LinearEyeDepth(SHADERGRAPH_SAMPLE_SCENE_DEPTH(PixelUVs));
 
-
-                // filteredNormals -= SAMPLE_TEXTURE2D(viewNormals.tex,viewNormals.samplerstate, PixelUVs).rgb;
-                filteredNormals -= viewNormals.tex.SampleLevel(ss, PixelUVs, 0).rgb;
-                filteredDepth -= SAMPLE_DEPTH_TEXTURE(ss,PixelUVs, 0);
-            } else {
-                filteredNormals += viewNormals.tex.SampleLevel(ss, UV, 0).rgb * centerWeight;
-                filteredDepth += SAMPLE_DEPTH_TEXTURE(ss,UV, 0) * centerWeight;
-            }
-    }
-
-    /*
-    for ( y = -HalfKernelSize; y <= HalfKernelSize; y++) {
-        for ( x = -HalfKernelSize; x <= HalfKernelSize; x++) {
-            if (x != 0.0 || y != 0.0){
-                PixelUVs = UV + TexelSize * float2(x,y);
-
-                filteredNormals -= SAMPLE_TEXTURE2D(viewNormals.tex,viewNormals.samplerstate, PixelUVs).rgb;
-                filteredDepth -= SHADERGRAPH_SAMPLE_SCENE_DEPTH(PixelUVs);
-            } else {
-                filteredNormals += SAMPLE_TEXTURE2D(viewNormals.tex,viewNormals.samplerstate, UV).rgb * centerWeight;
-                filteredDepth += SHADERGRAPH_SAMPLE_SCENE_DEPTH(UV);
-            }
+        if (x != 0.0 || y != 0.0) {
+            filteredNormals -= sN;
+            filteredDepth -= sD;
+        } else {
+            filteredNormals += sN * centerWeight;
+            filteredDepth += sD * centerWeight;
         }
     }
-    */
-
 
     filteredDepth /= centerWeight;
     filteredNormals /= centerWeight;
     Matte = float4(filteredNormals, filteredDepth);
-
 }
 
 #endif
