97f0fded34416a8607d2a38e9913a8e6370837a6
Hylu <roylu99@gmail.com>
2024-08-08 12:41:33 -0400

    Create script

---
+++ Assets/Game/Scripts/Camera/CameraTransitionSequence.cs
@@ -0,0 +1,31 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class CameraTransitionSequence : MonoBehaviour
+{
+    public float duration = 5;
+    public CinemachineVirtualCamera targetCamera;
+
+    private CinemachineVirtualCamera currentCamera;
+
+    public void Transition()
+    {
+        currentCamera = CameraManager.ActiveCineCamera;
+        StartCoroutine(StartTransition());
+    }
+
+    private IEnumerator StartTransition()
+    {
+        StartCoroutine(UIManager.Instance.gladeVillageIntroMenu.FadeIn(2.0f));
+        InputManager.Instance.TogglePlayerControls(false);
+        CameraManager.Instance.ChangeVirtualCamera(targetCamera);
+        yield return new WaitForSecondsRealtime(duration);
+        CameraManager.Instance.ChangeVirtualCamera(currentCamera);
+        InputManager.Instance.TogglePlayerControls(true);
+        StartCoroutine(UIManager.Instance.gladeVillageIntroMenu.FadeOut(2.0f));
+
+    }
+}
