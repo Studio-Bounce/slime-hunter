865ccc19e9d4d1801827cb8e4bbf9f49fde84ee2
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2024-08-16 00:43:49 -0400

    Crude version of alchemist quest's end

---
+++ Assets/Game/Dialogues/alchemist-mylo.ink
@@ -0,0 +1,23 @@
+-> alchemist
+
+=== alchemist ===
+0: "Thank you traveller for killing the slimes!"
+* [Continue]
+-> mylo1
+
+=== mylo1 ===
+1: "Are you the alchemist?"
+* [Continue]
+-> alchemist1
+
+=== alchemist1 ===
+0: "Yes."
+* [Continue]
+-> need_spells
+
+=== need_spells ===
+1: "I am on a journey to find my mentor. I need spells for killing slimes."
+* [Continue]
+0: "Let's go to my alchemy shop. I will cook some spells for you there. Follow me."
+
+-> DONE
--- Assets/Game/Scripts/_OneOff/MoveBridge.cs
+++ Assets/Game/Scripts/_OneOff/MoveBridge.cs
@@ -50,7 +50,10 @@ public class MoveBridge : MonoBehaviour
             transform.rotation = Quaternion.Lerp(initialRotation, finalTransform.rotation, t);
             transform.localScale = Vector3.Lerp(initialScale, finalTransform.localScale, t);
 
-            rotateWheel.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+            if (rotateWheel != null)
+            {
+                rotateWheel.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+            }
 
             yield return null;
         }
