8c09c6816e1ccd60924b80a51dab6ce1942a1665
Hylu <roylu99@gmail.com>
2024-08-16 02:36:40 -0400

    Add input hints and exit button

--- Assets/Game/Scripts/Managers/InputManager.cs
+++ Assets/Game/Scripts/Managers/InputManager.cs
@@ -32,6 +32,8 @@ public class InputManager : Singleton<InputManager>
     // Used for dash tutorial
     public Action<InputContext> OnDashAction;
 
+    public event Action exitEvent = delegate { };
+
     public Vector2 Movement { get { return _movement; } }
 
     private void Awake()
